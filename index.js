e0902ee804368a699a0164ce862b98cb0f2f74fc
Adielson Medeiros <medeirosadielson@gmail.com>
2023-07-01 00:29:36 -0300

    att

--- index.js
+++ index.js
@@ -6,28 +6,62 @@ server.use(express.json())
 //localhost:3000/curso
 const cursos = ['Node JS', 'Java script', 'React Native']
 
+//MIDDLEWARE GLOBAL
+server.use((req,res, next)=>{
+    console.log(`Url chamada: ${req.url}`)
+    return next();
+});
+
+
+
+function checkCurso(req, res, next){
+    if(!req.body.name){
+        return res.status(400).json({ error: "nome do curso obrigatorio"})
+    }
+    return next();
+}
+
+
+
+function checkIndexCurso(req, res, next){
+    const curso = cursos[req.params.index]
+    if(!curso){
+        return res.status(400).json({ error: "o curso nao existe"})
+    }
+    req.curso = curso;
+
+    return next();
+}
+
+
+
 server.get('/cursos', (req,res)=>{
     return res.json(cursos);
 })
 
 
-server.get('/cursos/:index', (req,res)=>{
-    const {index} = req.params;
-    return res.json(cursos[index]);
+
+
+server.get('/cursos/:index',checkIndexCurso, (req,res)=>{
+    return res.json(req.curso);
 });
 
 
+
+
 //criando um novo curso (POST)
-server.post ('/cursos', (req,res)=>{
+server.post ('/cursos',checkCurso , (req,res)=>{
     const {name} = req.body;
     cursos.push(name);
 
     return res.json(cursos)
 })
 
+
+
 //atualizando um curso (PUT)
 
-server.put('/cursos/:index', (req,res)=>{
+server.put('/cursos/:index',checkCurso, checkIndexCurso,(req,res)=>{
     const {index} = req.params;
     const { name } = req.body;
 
@@ -35,9 +69,11 @@ server.put('/cursos/:index', (req,res)=>{
     return res.json(cursos);
 })
 
+
+
 //excluindo cursos existentes
 
-server.delete('/cursos/:index', (req,res)=>{
+server.delete('/cursos/:index',checkIndexCurso, (req,res)=>{
     const {index} = req.params;
     cursos.splice(index, 1)
     return res.json({message: "curso deletado com sucesso"})
